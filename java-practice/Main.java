ca113087470dd66300797052acdbcdc5ff59dc7f
komal-sabertooth <gohade.k@sabertoothtech.in>
2024-03-02 14:42:59 +0530

    changes

---
+++ java-practice/Main.java
@@ -0,0 +1,9 @@
+// https://www.w3schools.com/java/java_getstarted.asp
+
+// ##### 
+
+public class Main {
+    public static void main(String[] args) {
+       System.out.println("Hello world");   
+    }
+}
